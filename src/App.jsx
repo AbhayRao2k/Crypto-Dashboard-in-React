48b95b295e37183dfa8840b9555b33a0d8763881
Abhay Rao <abhayrao2k@gmail.com>
2024-10-04 11:51:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Support from "./pages/Support/Support";
+import Transactions from "./pages/Transactions/Transactions";
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+      <Support />
+      <Transactions />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const Sidenav = () => {
+  return <Box as="aside">Sidenav</Box>;
+};
+
+export default Sidenav;
